2f4112c4ca7882272c5fb5e55f3c6c56eebd244a
andi49 <46962871+andi49@users.noreply.github.com>
2025-12-03 08:39:48 +0100

    update

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
 const fetchAPI = async () => {
     try { 
-const URL = `https://opentdb.com/api.php?${amount}=10&category=9&difficulty=${difficultyGame}&type=multiple`
+// const URL = `https://opentdb.com/api.php?${amount}=10&category=9&difficulty=${difficultyGame}&type=multiple`
+const URL = `https://opentdb.com/api_category.php`
 const response = await fetch(URL)
 const data = await response.json()
 
-return data
+return data.trivia_categories
 } catch (error) {
     console.log(error);
   }
 
 }
+function displayCotergi (arr) {
+const getCaterogy = document.querySelector('#category')
+
+  for (const category of arr) {
+    const option = document.createElement("option");
+    option.innerText = category;
+    console.log(category);
+
+   option.innerText = category.name    // <-- Correct text
+    option.value = category.id          // <-- Correct value
+
+    getCaterogy.append(option);
+  }
+
+}
+fetchAPI().then(displayCotergi)
+
+
+
 
 const getName = document.querySelector('#name')
 const amount = document.querySelector('#number')
-const category = document.querySelector('#category')
-const difficultyGame = document.querySelector('#difficulty')
+
