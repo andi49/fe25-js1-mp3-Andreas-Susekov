c41c78b9a2223e29cef8fa3b0ef4dec74f059f97
andi49 <46962871+andi49@users.noreply.github.com>
2025-12-10 09:03:09 +0100

    Updata main.js

--- main.js
+++ main.js
@@ -1,108 +1,108 @@
 import { fetchMyApi, fetchCatergoris } from "./js/api.js";
 
-const getCaterogy = document.querySelector('#category')
-const getdifficulty = document.querySelector('#difficulty')
-const getNumberEl = document.querySelector('#number')
+const getCaterogy = document.querySelector("#category");
+const getdifficulty = document.querySelector("#difficulty");
+const getNumberEl = document.querySelector("#number");
 
+let gameScore = 0
 
+getNumberEl.addEventListener("input", () => {
+  const num = getNumberEl.value;
+});
 
-getNumberEl.addEventListener('input', () => {
-  const num = getNumberEl.value
-})  
+function displayCotergi(cat) {
+  cat.forEach((category) => {
+    const option = document.createElement("option");
 
-
-
-function displayCotergi (cat) {
-
- cat.forEach(category => {
-  const option = document.createElement("option");
-
-    option.innerText = category.name    
-    option.value = category.id         
+    option.innerText = category.name;
+    option.value = category.id;
 
     getCaterogy.append(option);
- });
+  });
+}
+
+const difficultyChose = [
+  { id: "easy", name: "Easy" },
+  { id: "normal", name: "Normal" },
+  { id: "hard", name: "Hard" },
+];
+
+function displayDifficulty(def) {
+  def.forEach((difficultyValue) => {
+    const optionTwo = document.createElement("option");
+    optionTwo.innerText = difficultyValue.name;
+    optionTwo.value = difficultyValue.id;
+    getdifficulty.append(optionTwo);
+  });
 }
 
 //  ---------------- ---------------- ---------------- ---------------- ---------------- ---------------- ----------------
 
-const difficultyChose = [
-  {id: 'easy', name: 'Easy' },
-    {id: 'normal', name: 'Normal' },
-      {id: 'hard', name: 'Hard' }
-]
+fetchCatergoris().then(displayCotergi);
+displayDifficulty(difficultyChose);
 
-
-function displayDifficulty (def) {
-
-def.forEach(difficultyValue => {
-  const optionTwo = document.createElement("option");
-  optionTwo.innerText = difficultyValue.name;
-  optionTwo.value = difficultyValue.id;
-  getdifficulty.append(optionTwo);
-});
-}
-
-getdifficulty.addEventListener('change', (event) => {
-  const selectedValue = event.target.value
-  // console.log('You selected', selectedValue)
-})
-
-fetchCatergoris().then(displayCotergi)
-displayDifficulty(difficultyChose)
-
-
-document.querySelector("form").addEventListener("submit",  async (event) => {
+document.querySelector("form").addEventListener("submit", async (event) => {
   event.preventDefault();
 
- const number = document.querySelector('#number').value
- const category = document.querySelector('#category').value
- const difficulty = document.querySelector('#difficulty').value
+  const number = document.querySelector("#number").value;
+  const category = document.querySelector("#category").value;
+  const difficulty = document.querySelector("#difficulty").value;
 
- console.log({ number, category, difficulty });
- const callAPI = await fetchMyApi(number, category, difficulty)
+  console.log({ number, category, difficulty });
+  const callAPI = await fetchMyApi(number, category, difficulty);
 
- document.querySelector("form").style.display = "none";
 
- function getQuestionFromAPI() {
 
-  const getQuest = document.getElementById('question')
 
-  getQuest.innerText = callAPI[0].question
-//  const getCorrectAnswer = document.getElementById('answers')
-//  getCorrectAnswer.innerText = callAPI[0].correct_answer
-}
 
-function shuffleArrays() {
+  function shuffleArrays() {
 
-let answersOne = callAPI[0].incorrect_answers
-let answerTwo = callAPI[0].correct_answer
-console.log(answersOne, answerTwo) 
+      document.querySelector("form").style.display = "none";
 
-let allAnswers = [...answersOne, answerTwo]
-let shuffleArray = allAnswers.sort(() => Math.random() - 0.5)
+  
 
-console.log(shuffleArray)
- const answerButtonsDiv = document.getElementById('answerButtons')
+      const showQuest = document.getElementById("question");
 
-shuffleArray.forEach((answer) => {
-  const button = document.createElement("button");
-  button.textContent = answer;
-  button.classList.add("answer-button"); 
-  button.value = answer; 
+      for (let i = 0; i < callAPI.length; i++) {
+        
+        const getQuestion = callAPI[i].question;
+        showQuest.innerText = getQuestion
+    }
+   
+  
+    let answersOne = callAPI[i].incorrect_answers;
+    let answerTwo = callAPI[i].correct_answer;
+    console.log(answersOne, answerTwo);
 
-  answerButtonsDiv.appendChild(button);
+    let allAnswers = [...answersOne, answerTwo];
+    let shuffleArray = allAnswers.sort(() => Math.random() - 0.5);
+
+    console.log(shuffleArray);
+    const answerButtonsDiv = document.getElementById("answerButtons");
+
+    shuffleArray.forEach((answer) => {
+      const buttonFake = document.createElement("button");
+      buttonFake.textContent = answer;
+      buttonFake.classList.add("answer-button");
+      buttonFake.value = answer;
+
+      answerButtonsDiv.appendChild(buttonFake);
+
+      buttonFake.addEventListener('click', () => {
+        console.log('I have been clicked')
+      }) 
+    });
+
+  }
+
+
+  shuffleArrays();
+ 
+
+  console.log(callAPI);
 });
 
-}
 
 
-getQuestionFromAPI()
-shuffleArrays()
-
-console.log(callAPI)
-})
-
-
-
-
+ //  const getCorrectAnswer = document.getElementById('answers')
+    //  getCorrectAnswer.innerText = callAPI[0].correct_answer
